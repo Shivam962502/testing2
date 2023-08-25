b262d7247077d83888ac4f0929e7e5f3e3f250f3
Shivam962502 <shivamg@sovtechnologies.com>
2023-08-25 17:03:47 +0530

    fith commit

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-// branchB.js
-const branchB = "This is from branch B";
-console.log(branchB);
+// file.js (feature/branch2)
+function calculateTotal(items) {
+    let total = 0;
+    for (const item of items) {
+        total += item.quantity * item.price; // Updated calculation
+    }
+    return total;
+}
+
+const cartItems = [
+    { name: 'Item 1', price: 10, quantity: 2 }, // Quantity added
+    { name: 'Item 2', price: 20, quantity: 3 }, // Quantity added
+    // ... more items
+];
+
+console.log('Total:', calculateTotal(cartItems));
+
+// hello bite
