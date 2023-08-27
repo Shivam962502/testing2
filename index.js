bb29e75e905d94d02f7bd8d560e044bb6e6b438b
Shivam962502 <shivamg@sovtechnologies.com>
2023-08-27 23:12:37 +0530

    first commit for second Branch

--- index.js
+++ index.js
@@ -1,18 +1,1 @@
-// file.js (feature/branch2)
-function calculateTotal(items) {
-    let total = 0;
-    for (const item of items) {
-        total += item.quantity * item.price; // Updated calculation
-    }
-    return total 
-}
-
-const cartItems = [
-    { name: 'Item 1', price: 10, quantity: 2 }, // Quantity added
-    { name: 'Item 2', price: 20, quantity: 3 }, // Quantity added
-    // ... more items
-];
-console.log("HIII WOrld")
-console.log('Total:', calculateTotal(cartItems));
-
-// hello bite
+console.log("This is a second branch Code ")
